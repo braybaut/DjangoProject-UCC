0bcd9b1939a418ead90271816b57cb7aa745c223
Brayan Bautista <brayan.bautista@seaq.co>
2017-05-16 19:13:35 -0500

    django files

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/braybaut/Documents/DocumentsBray/Documentos/Documentos/Proyectos/DjangoProject-UCC/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
